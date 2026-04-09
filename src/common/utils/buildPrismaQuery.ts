1f9dac6305434dee5b77e1b01693c5b99f2ca6fd
duccsquared <mariosusanto161@gmail.com>
2026-04-09 21:48:59 +0700

    added relational handling to retrieve entities with subentities

--- src/common/utils/buildPrismaQuery.ts
+++ src/common/utils/buildPrismaQuery.ts
@@ -1,8 +1,9 @@
 import { QueryDTO } from "../dto/query.dto";
 
 export function buildPrismaQuery(query: QueryDTO) {
-  const { page = 1, limit = 10, sortBy, order = 'asc', ...filters } = query;
+  const { page = 1, limit = 10, sortBy, order = 'asc', include, ...filters } = query;
 
+  // filter handling
   const where: Record<string, any> = {};
 
   for (const key in filters) {
@@ -14,8 +15,19 @@ export function buildPrismaQuery(query: QueryDTO) {
     }
   }
 
+  // relational handling
+  const includeObj: Record<string, boolean> = {};
+  if(include != null && typeof include === 'string') {
+    const relations: string[] = include.split(',');
+
+    relations.forEach((rel: string) => {
+      includeObj[rel] = true;
+    });
+  }
+
   return {
     where,
+    include: includeObj,
     skip: (page - 1) * limit,
     take: Number(limit),
     orderBy: sortBy
