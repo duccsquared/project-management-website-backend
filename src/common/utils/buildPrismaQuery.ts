8950ae2247e671cfc363afee0c2147dd72e6ae61
Mario Susanto <mariosusanto161@gmail.com>
2026-05-26 21:06:16 +0700

    implemented advanced filtering for retrievals

--- src/common/utils/buildPrismaQuery.ts
+++ src/common/utils/buildPrismaQuery.ts
@@ -4,16 +4,38 @@ export function buildPrismaQuery(query: QueryDTO) {
   const { page = 1, limit = 10, sortBy, order = 'asc', include, ...filters } = query;
 
   // filter handling
-  const where: Record<string, any> = {};
+  const where: Record<string, unknown> = {};
 
   for (const key in filters) {
-    if (filters[key]) {
-      where[key] = {
-        contains: filters[key] as string,
-        mode: 'insensitive', // case-insensitive search
-      };
+    const value: unknown = filters[key] as unknown;
+
+    // Split field + operator
+    const [field, operator] = key.split('_');
+
+    // Default operator
+    // valid operators: equals, contains gt, gte, lt, lte, in, not
+    const prismaOperator = operator || 'equals';
+
+    // Handle nested relational fields
+    if (field.includes('.')) {
+      buildNestedRelation(where, field, prismaOperator, value);
+      continue;
     }
+
+    // Handle comma-separated arrays
+    const parsedValue =
+      prismaOperator === 'in' && typeof value === 'string'
+        ? value.split(',')
+        : value as string;
+
+    if (!Object.prototype.hasOwnProperty.call(where, field)) {
+      // initialize as a record for nested operators
+      (where as Record<string, Record<string, unknown>>)[field] = {};
+    }
+
+    ((where as Record<string, Record<string, unknown>>)[field])[prismaOperator] = parsedValue;
   }
+
 
   // relational handling
   const includeObj: Record<string, boolean> = {};
@@ -33,5 +55,25 @@ export function buildPrismaQuery(query: QueryDTO) {
     orderBy: sortBy
       ? { [sortBy]: order }
       : undefined,
+  };
+}
+
+function buildNestedRelation(
+  where: Record<string, any>,
+  path: string,
+  operator: string,
+  value: any,
+) {
+  const keys = path.split('.');
+
+  let current = where;
+
+  for (let i = 0; i < keys.length - 1; i++) {
+    current[keys[i]] = current[keys[i]] as Record<string, any> || {};
+    current = current[keys[i]] as Record<string, any>;
+  }
+
+  current[keys[keys.length - 1]] = {
+    [operator]: value as string,
   };
 }
