4b982feae04ac170b138aee01be6c83a5e20d1dd
Mario Susanto <mariosusanto161@gmail.com>
2026-05-29 19:37:13 +0700

    renamed project folder to projects folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-import { ProjectsModule } from './modules/project/projects.module';
+import { ProjectsModule } from './modules/projects/projects.module';
 import { ProjectMembersModule } from './modules/projectMembers/projectMembers.module';
 import { TestModule } from './modules/test/test.module';
 
